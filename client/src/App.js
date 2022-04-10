49b1e2412df00ae59dfef833bfbc62cc7ccfcdf2
Karim-Nabarawi <K.nabarawi@gmail.com>
2022-04-10 10:38:43 +0200

    - fixed double post

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Card from "./components/card/Card.js";
 import Delivery from "./components/delivery/Delivery.js";
@@ -8,107 +8,106 @@ import imgWater from "./img/water.svg";
 import imgSugar from "./img/sugar.svg";
 
 function App() {
-    const ingredients = [
-        {
-            id: "0",
-            name: "Espresso",
-            minAmount: 1,
-            maxAmount: 5,
-            img: imgEspresso,
-        },
-        {
-            id: "1",
-            name: "Milk",
-            minAmount: 0,
-            maxAmount: 4,
-            img: imgMilk,
-        },
-        {
-            id: "2",
-            name: "Water",
-            minAmount: 0,
-            maxAmount: 3,
-            img: imgWater,
-        },
-        {
-            id: "3",
-            name: "Sugar",
-            minAmount: 0,
-            maxAmount: 2,
-            img: imgSugar,
-        },
-    ];
+  const ingredients = [
+    {
+      id: "0",
+      name: "Espresso",
+      minAmount: 1,
+      maxAmount: 5,
+      img: imgEspresso,
+    },
+    {
+      id: "1",
+      name: "Milk",
+      minAmount: 0,
+      maxAmount: 4,
+      img: imgMilk,
+    },
+    {
+      id: "2",
+      name: "Water",
+      minAmount: 0,
+      maxAmount: 3,
+      img: imgWater,
+    },
+    {
+      id: "3",
+      name: "Sugar",
+      minAmount: 0,
+      maxAmount: 2,
+      img: imgSugar,
+    },
+  ];
 
-    const coffee = {
-        Espresso: 1,
-        Milk: 1,
-        Water: 1,
-        Sugar: 1,
-        date: null,
-    };
+  const coffee = {
+    Espresso: 1,
+    Milk: 1,
+    Water: 1,
+    Sugar: 1,
+    date: null,
+  };
 
-    const [caffieneHistory, setCaffieneHistory] = useState(0);
-    const [customCoffee, setCustomCoffee] = useState(coffee);
-    let sendFlag = false;
+  const [caffieneHistory, setCaffieneHistory] = useState(0);
+  const [customCoffee, setCustomCoffee] = useState(coffee);
+  let sendFlag = false;
 
-    async function sendData(submitData) {
-        if (sendFlag) return;
-        sendFlag = true;
-        let tempCaffiene = caffieneHistory;
-        const result = await fetch("http://localhost:5000/postdata", {
-            method: "POST",
-            headers: {
-                "Content-type": "application/json",
-            },
-            body: JSON.stringify(submitData),
-        });
-        tempCaffiene = tempCaffiene + customCoffee.Espresso;
-        setCaffieneHistory(tempCaffiene);
-        setCustomCoffee({ ...coffee }); //reset coffee
-        // console.log(result);
-        if (result.status === 200) {
-            console.log("Post complete");
-            getData();
-        }
-        sendFlag = false;
+  async function sendData(submitData) {
+    console.log(sendFlag);
+    if (sendFlag) return;
+    sendFlag = true;
+    let tempCaffiene = caffieneHistory;
+    const result = await fetch("http://localhost:5000/postdata", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(submitData),
+    });
+    tempCaffiene = tempCaffiene + customCoffee.Espresso;
+    setCaffieneHistory(tempCaffiene);
+    setCustomCoffee({ ...coffee }); //reset coffee
+    // console.log(result);
+    if (result.status === 200) {
+      console.log("Post complete");
+      getData();
     }
+    sendFlag = false;
+  }
 
-    async function getData() {
-        console.log(caffieneHistory);
-        const result = await fetch("http://localhost:5000/getdata"); //fetch data
-        let dataJson = await result.json();
-        console.log(dataJson);
-        setCaffieneHistory(dataJson); //set state to be marked
-    }
+  async function getData() {
+    console.log(caffieneHistory);
+    const result = await fetch("http://localhost:5000/getdata"); //fetch data
+    let dataJson = await result.json();
+    console.log(dataJson);
+    setCaffieneHistory(dataJson.sum); //set state to be marked
+  }
 
-    return (
-        <div className="App">
-            <p className="titleText">Espresso Order and Delivery</p>
-            {ingredients.map((data) => (
-                <Card
-                    key={data.id}
-                    name={data.name}
-                    minAmount={data.minAmount}
-                    maxAmount={data.maxAmount}
-                    img={data.img}
-                    customCoffee={customCoffee}
-                    setCustomCoffee={setCustomCoffee}
-                />
-            ))}
-            <Delivery
-                date={coffee.date}
-                setCustomCoffee={setCustomCoffee}
-                customCoffee={customCoffee}
-            />
-            <button className="appBtn" onClick={() => sendData(customCoffee)}>
-                Submit
-            </button>
-            {/* do a post and save data */}
-            <p className="caffieneHistory">
-                caffiene history: {caffieneHistory} espresso shots
-            </p>
-        </div>
-    );
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div className="App">
+      <p className="titleText">Espresso Order and Delivery</p>
+      {ingredients.map((data) => (
+        <Card
+          key={data.id}
+          name={data.name}
+          minAmount={data.minAmount}
+          maxAmount={data.maxAmount}
+          img={data.img}
+          customCoffee={customCoffee}
+          setCustomCoffee={setCustomCoffee}
+        />
+      ))}
+      <Delivery date={coffee.date} setCustomCoffee={setCustomCoffee} customCoffee={customCoffee} />
+      <button className="appBtn" onClick={() => sendData(customCoffee)}>
+        Submit
+      </button>
+      {/* do a post and save data */}
+      <p className="caffieneHistory">caffiene history: {caffieneHistory} espresso shots</p>
+    </div>
+  );
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -6,14 +6,14 @@ const port = 5000;
 const whitelist = ["http://localhost:3000"];
 
 const corsOptions = {
-    origin: function (origin, callback) {
-        if (!origin || whitelist.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
-    credentials: true,
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
 };
 
 const app = express();
@@ -21,40 +21,40 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 app.get("/getdata", async (req, res) => {
-    try {
-        const buffer = fs.readFileSync("datajson.json");
-        dataJSON = JSON.parse(buffer);
-        console.log("Sending data");
-        let sum = 0;
-        dataJSON.forEach((element) => {
-            sum = sum + element.Espresso;
-        });
-        console.log(sum);
-        res.send(sum);
-    } catch (e) {
-        res.send(e);
-    }
+  try {
+    const buffer = fs.readFileSync("datajson.json");
+    dataJSON = JSON.parse(buffer);
+    console.log("Sending data");
+    let sum = 0;
+    dataJSON.forEach((element) => {
+      sum = sum + element.Espresso;
+    });
+    console.log(sum);
+    res.send({ sum: sum });
+  } catch (e) {
+    res.send(e);
+  }
 });
 
 app.post("/postdata", async (req, res) => {
-    let incomingJSON = req.body;
-    try {
-        const buffer = await fs.readFileSync("datajson.json");
-        dataJSON = await JSON.parse(buffer);
-    } catch (e) {
-        res.send(e);
+  let incomingJSON = req.body;
+  try {
+    const buffer = fs.readFileSync("datajson.json");
+    dataJSON = await JSON.parse(buffer);
+  } catch (e) {
+    res.send(e);
+  }
+  const saveData = [...dataJSON, incomingJSON];
+  console.log(saveData);
+  fs.writeFile("datajson.json", JSON.stringify(saveData), (err) => {
+    if (err) {
+      res.render("Error writing data");
+      return;
     }
-    const saveData = [...dataJSON, incomingJSON];
-    console.log(saveData);
-    fs.writeFile("datajson.json", JSON.stringify(saveData), (err) => {
-        console.log("Saved Data");
-        if (err) {
-            res.render("Error writing data");
-            return;
-        }
-    });
+  });
+  res.send("Saved Data");
 });
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
